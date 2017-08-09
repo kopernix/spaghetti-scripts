d2356c3a5871cc2a6a34be43258ac4745bc2984c
kopernix <joan@kopernix.com>
2017-08-09 17:39:03 +0200

    Bugs

--- issue_brander/unistall.sh
+++ issue_brander/unistall.sh
@@ -3,4 +3,4 @@
 /bin/rm /usr/local/bin/get-ip-address --verbose ;
 /bin/rm /etc/network/if-up.d/addip2issue --verbose ;
 /bin/rm /etc/issue-original --verbose ;
-/bin/cp -a /etc/issue.backup_issue_brander /etc/issue.net --verbose ;
+/bin/cp -a /etc/issue.backup_issue_brander /etc/issue --verbose ;

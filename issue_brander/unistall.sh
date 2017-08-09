7f6d9c5d3e0c9c35ec7deeedb9979b8bcc1e3dfd
kopernix <joan@kopernix.com>
2017-08-09 17:36:27 +0200

    Create unistall.sh

---
+++ issue_brander/unistall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -e
+
+/bin/rm /usr/local/bin/get-ip-address --verbose ;
+/bin/rm /etc/network/if-up.d/addip2issue --verbose ;
+/bin/rm /etc/issue-original --verbose ;
+/bin/cp -a /etc/issue.backup_issue_brander /etc/issue.net --verbose ;
